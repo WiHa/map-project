4b8864826e4935ba4127f4db8b3a9888b7b8a0d0
WiHa <winnie.hanschen@gmail.com>
2015-05-26 19:20:29 +0200

    markers added

--- src/entry.jsx
+++ src/entry.jsx
@@ -12,6 +12,10 @@ import femGayHist from '../project-images/gay-female_2.png';
 import femGayHistChoice from '../project-images/gay-female_1.png';
 import warHist from '../project-images/war_1.png';
 import warHistChoice from '../project-images/war_2.png';
+import domHist from '../project-images/smiley_happy_1.png';
+import domHistChoice from '../project-images/smiley_happy_2.png';
+import slaveHist from '../project-images/torture_1.png';
+import slaveHistChoice from '../project-images/torture_2.png';
 
 
 
@@ -25,6 +29,16 @@ var Map = React.createClass({
 
 
     var markers = [
+    		//Alan Turing Pardon
+    		[ -0.124809, 51.499480, 'On December 24th 2013 Alan Turing was pardoned for his conviction for gross indecency for which he was chemically castrated. His pardon comes a mere 59 years after his death, and only for him. Not for the other 49.000 men who have been convicted for homosexual relations.<br><a href="https://www.change.org/p/british-government-pardon-all-of-the-estimated-49-000-men-who-like-alan-turing-were-convicted-of-consenting-same-sex-relations-under-the-british-gross-indecency-law-only-repealed-in-2003-and-also-all-the-other-men-convicted-under-other-uk-anti-gay" target="_blank">People are striving to get that done </a> ', femGayHist, femGayHistChoice],
+    		// Rembrandt House
+    		[4.901235, 52.369369, ' The Rembrandt House<br><iframe width="560" height="315" src="https://www.youtube.com/embed/tVBsi5LFQwc" frameborder="0" allowfullscreen></iframe>', domHist, domHistChoice],
+    		//Gay Monument Amsterdam
+    		[ 4.884913, 52.374420, 'The idea of a permanent memorial to gay and lesbian victims of persecution dated from 1970, when gay activists were arrested for attempting to place a wreath at the National War Memorial on Dam Square in the centre, as is done to commemorate victims of WWII on Mey 4th of every year. The wreath was removed by police and denounced as a disgrace. After that, a campaign was started that resulted in the worlds first monument in public space, that is a memorial to the persecution of homosexuals. <br><a href="http://en.wikipedia.org/wiki/Homomonument" target="_blank"> Wikipedia </a>', femGayHist, femGayHistChoice],
+    		// Plaquette aanslag bevolkingsregister
+    		[ 4.911924, 52.366478, 'On March 27, 1943 members of a resistance group, dressed as police officers, entered the civil register office and set fire to the building. Twelve people involved in the group known as the Artists Reistance were executed non July 1st. What is less known is that most members of this group were gay, such as one of the leaders of the group Willem Arondéus.', femGayHist, femGayHistChoice ],
+    		// Anne Frank House
+    		[ 4.883989, 52.375184, 'Something about the Anne Frank House.', domHist, domHistChoice ],
     		//KVV
             [ 4.891827, 52.365070, '<iframe width="420" height="315" src="https://www.youtube.com/embed/yd-t7OkBF3g" frameborder="0" allowfullscreen></iframe> <button className="selected"> Select </button>', femHist, femHistChoice  ],
             //1st vrouwelijke politieagenten
@@ -50,8 +64,21 @@ var Map = React.createClass({
 			//Education for the Blind.
             [ 4.819959, 52.378813, 'In 1808 the Institute for the Education of the Blind is founded in Amsterdam, the first school for the blind.', disHist, disHistChoice ],
             //River Kwai
-            [ 99.532811, 14.022780, 'Well done! You looked beyond the frame you were given. This is the River Kwai, famous for its railway bridge built by Japan in WWII. But did you know that Thailand was never occupied? And that it was the only country in South East Asia to not be colonised?<br><a href="www.stickmanbangkok.com/ReadersSubmissions2014/reader8591.htm" target="_blank">Why Thailand Was Never Colonized</a>', warHist, warHistChoice ]
-
+            [ 99.532811, 14.022780, 'Well done! You looked beyond the frame you were given. This is the River Kwai, famous for its railway bridge built by Japan in WWII. But did you know that Thailand was never occupied? And that it was the only country in South East Asia to not be colonised?<br><a href="www.stickmanbangkok.com/ReadersSubmissions2014/reader8591.htm" target="_blank">Why Thailand Was Never Colonized</a>', warHist, warHistChoice ],
+            // Slave Compensation
+            [ 4.89456, 52.361602, 'Joan Muijsken first lived on the Singel 274, later on this address, Prinsengracht 712. With the abolishment of slavery he was one of the people to receive 300 guilders compensation per slave, owning as many as 128 slaves at that time.<br> <a href="https://www.google.com/maps/d/viewer?mid=z_50lGRHy2eM.kOvGbMjogrkc&msa=0&hl=nl&ie=UTF8&t=m&ll=52.372874,4.894152&spn=0.043441,0.077162&source=embed" target="_blank">A full map of the slave owners in Amsterdam that recieved compensation</a>', slaveHist, slaveHistChoice],
+            //Deportation of handicapped
+            [4.90974, 52.367695, 'On February 3 1943 the A.C. Wertheimhuis, a care home, was cleared. How many people were taken, and what happened to them remains unclear. What we do know is that between 1939 and 1945 hundreds of thousands of children and adults with disabilities were systematically killed by the nazi-regime, as part of a so called "euthanasia-programme".', disHist, disHistChoice],
+            //Golden Bend
+            [4.889367, 52.366376, 'Since 2010 the Amsterdam Canals have been placed on the Unesco World Heritage list. Part of it is this, the Golden Bend, in the Golden Age (and perhaps still) the most prestigious part of the canals. The rich people that lived there made most money by owning plants in Suriname. In 1814 trade in slaves is abolished, but it takes until July 1st 1863 until slavery is abolished entirely, because economic impact of abolishment is too great for those in power. And then slaves are forced to work until 1873 due to discussions about the height of compensation.', slaveHist, slaveHistChoice ],
+            //Slave plays
+            [4.881924, 52.363670, 'At the end of the 18th century many books were published against slavery. Plays with the same topic were staged in the Stadsschouwburg. "Monzongo" or "The Royal Slave" was one of them, but an adaptation of this play was quickly forbidden in 1810. Voices against slavery would only rise again in the 1840s.', slaveHist, slaveHistChoice],
+            //Mayor's residence
+            [4.893444, 52.364861, "Now the official residence of the mayor of Amsterdam, formerly the house of Paulus Godin(1618-1690), very active in slavery and one of the richest people in the city in his day. Mayor Job Cohen (mayor from 2001-2010) had a plaquette attached to the building reminding of the relationship this building has with slavery.", slaveHist, slaveHistChoice],
+            //Royal Palace
+            [4.891368, 52.373186, "Known as the Royal Palace of Amsterdam, this house is actually the former city's Town Hall that became a palace after Louis Napoleon became king of the country.<br> <a href='http://www.paleisamsterdam.nl/en/the-palace/history/napoleonic-palace/city-hall-becomes-napoleonic-palace'>Official Website</a>", domHist, domHistChoice ],
+            //Royal Palace
+            [4.891368, 52.373186, "The Town Hall, as it was before Louis Napoleon. The place where Amsterdam business was conducted in the Golden Age. The place where the West-Indian Company was founded, that had the exclusive rights to trade people in the Atlantic area. Most people in the WIC were also politically active as mayors, council members etc. The WIC, in the first 100 years of its existence, had already shipped a quarter of a million Africans across the ocean under force.", slaveHist, slaveHistChoice],
          ];
 
          //Loop through the markers array
