cfd632049af31ff51862ffbbb9f380770e8abafa
WiHa <winnie.hanschen@gmail.com>
2015-05-30 19:57:03 +0200

    about.html

---
+++ quotes.js
@@ -0,0 +1,71 @@
+
+
+
+var quotes=new Array()
+
+//change the quotes if desired. Add/ delete additional quotes as desired.
+
+quotes[0]='Essentially, tourism, as a search for meaning, with the lucid sociability it favours, the images it generates, is a graduated, encoded and untraumatising apprehension system of the external, of otherness. <i>-Rachid Amirou</i>'
+
+quotes[1]='Writing has nothing to do with meaning. It has to do with landsurveying and cartography, including the mapping of countries yet to come. <i>-Gilles Deleuze</i>'
+
+quotes[2]='Regular maps have few surprises: their contour lines reveal where the Andes are, and are reasonably clear. More precious, though, are the unpublished maps we make ourselves, of our city, our place, our daily world, our life; those maps of our private world we use every day; here I was happy, in that place I left my coat behind after a party, that is where I met my love; I cried there once, I was heartsore; but felt better round the corner once I saw the hills of Fife across the Forth, things of that sort, our personal memories, that make the private tapestry of our lives. <i>-Alexander McCall-Smith</i>'
+
+quotes[3]='A map does not just chart, it unlocks and formulates meaning; it forms bridges between here and there, between disparate ideas that we did not know were previously connected. <i>-Reif Larsen</i>'
+
+quotes[4]='To put a city in a book, to put the world on one sheet of paper -- maps are the most condensed humanized spaces of all...They make the landscape fit indoors, make us masters of sights we can\'t see and spaces we can\'t cover. <i>-Robert Harbison</i>'
+
+quotes[5]='Maps codify the miracle of existence <i>-Nicholas Crane</i>'
+
+quotes[6]='Through our maps, we willingly become a part of their boundaries. If our home is included, we feel pride, perhaps familiarity, but always a sense that this is ours. If it is not, we accept our roles as outsiders, though we may be of the same mind and culture. In this way, maps can be dangerous and powerful tools. <i>-Debbie Lee Wesselmann</i>'
+
+quotes[7]='Two important characteristics of maps should be noticed. A map is not the territory it represents, but, if correct, it has a similar structure to the territory, which accounts for its usefulness. <i>-Alfred Korzybski</i>'
+
+quotes[8]='Never had he contemplated an object as magnificent, as rich in emotion and meaning, as this 1/150,000-scale Michelin map of the Creuse and the Haute-Vienne. The essence of modernity, of scientific and technical apprehension of the world, was here combined with the essence of animal life. The drawing was complex and beautiful, absolutely clear, using only a small palette of colors. But in each of the hamlets and villages, represented according to their importance, you felt the thrill, the appeal, of human lives, of dozens and hundreds of souls—some destined for damnation, others for eternal life. <i>-Michel Houllebecq</i>'
+
+quotes[9]='There were waves of genocide that overcame indigenous populations of Oceania and do we have a library of books or films to tell our story? No. We have tourist hula shows and commercials where the “natives” tend to tourists like indentured servants with plastic, lifeless smiles. It’s not such a charming picture, is it? The truth is ugly, but so is ignorance or denial of such atrocities and pain. <i>-M.B. Dallocchio</i>'
+
+quotes[10]='Tradition means giving votes to the most obscure of all classes, our ancestors. It is the democracy of the dead. Tradition refuses to submit to that arrogant oligarchy who merely happen to be walking around. <i>-Gilbert K. Chesterton</i>'
+
+quotes[11]='History is not about dates and quotes and obscure provisos. History is about life. About change. About consequences. Cause and effect. It’s about the mystery of human nature. The mystery of time. And it isn’t just about politics and the military and social issues, which is almost always the way it’s taught. It’s about music and poetry and drama and science and medicine and money and love. <i>-David McCullough</i>'
+
+quotes[12]='History is the version of past events that people have decided to agree upon. <i>-Napoleon Bonaparte</i>'
+
+quotes[13]='I am what time, circumstance, history, have made of me, certainly, but I am also, much more than that. So are we all. <i>-James A. Baldwin</i>'
+
+quotes[14]='The dogmas of the quiet past are inadequate to the stormy present. The occasion is piled high with difficulty, and we must rise with the occasion. As our case is new, so we must think anew and act anew. <i>-Abraham Licoln</i>'
+
+quotes[15]='History is a cyclic poem written by time upon the memories of man. <i>-Percy Bysshe Shelley</i>'
+
+quotes[16]='History is a pack of lies about events that never happened told by people who weren\'t there. <i>-George Santayana</i>'
+
+quotes[17]='If society will not admit of woman\'s free development, then society must be remodeled. <i>-Elizabeth Blackwell</i>'
+
+quotes[18]='Neither the historian nor the cartographer can ever reproduce the reality they are trying to communicate to the reader of books or maps; they can but give a plan, a series of indications, of this reality. There are contrasting schemes for choosing from enormous numbers of geographic details. You may have a map in which every feature that can be named, every hill, brook, crossroads, is crowded in; or you may have a map in which many details are omitted in the effort to show the reader the lay of the land, the shape of the mountain systems, the relations of drainage, relief, communications, and so on. Both kinds are useful, depending on the needs of the user.<i>-Crane Brinton</i>'
+
+quotes[19]='History is nothing but geography over time, such as geography is nothing but history in space. <i>-Élisée Recluse</i>'
+
+quotes[20]='Digital mapping is about to change our world by documenting the real world, then integrating the information into our computers, phones, and lifestyles. Roll over, Mason and Dixon: spurred by space photography, global satellite position- ing, mobile phones, search engines and new ways of marketing information for the World Wide Web, the ancient art of cartography is now on the cutting edge.<i>-S. Levy</i>'
+
+quotes[21]='The good cartographer is both a scientist and an artist. He must have a thorough knowledge of his subject and model, the Earth…. He must have the ability to generalize intelligently and to make a right selection of the features to show. These are represented by means of lines or colors; and the effective use of lines or colors requires more than knowledge of the subject – it requires artistic judgement. <i>-Erwin Josephus Raisz</i>'
+
+quotes[22]='When our maps do not fit the territory, when we act as if our inferences are factual knowledge, we prepare ourselves for a world that isn’t there. If this happens often enough, the inevitable result is frustration and an ever-increasing tendency to warp the territory to fit our maps. We see what we want to see, and the more we see it, the more likely we are to reinforce this distorted perception, in the familiar circular and spiral feedback pattern. <i>-Harry L. Weinberg</i>'
+
+quotes[23]='The computer programmer is a creator of universes for which he alone is the lawgiver. No playwright, no stage director, no emperor, however powerful, has ever exercised such absolute authority to arrange a stage or field of battle and to command such unswervingly dutiful actors or troops. <i>-Joseph Weizenbaum</i>'
+
+quotes[24]='A computer is like a violin. You can imagine a novice trying ﬁrst a phonograph and then a violin. The latter, he says, sounds terrible. That is the argument we have heard from our humanists and most of our computer scientists. Computer programs are good, they say, for particular purposes, but they aren’t ﬂexible. Neither is a violin, or a typewriter, until you learn how to use it. <i>-Marvin Minsky</i>'
+
+quotes[25]='What\'s in your hands I think and hope is intelligence: the ability to see the machine as more than when you were ﬁrst led up to it that you can make it more.<i>-Alan J. Perlis</i>'
+
+quotes[26]='Programmers are isolated. They sit in their cubicle; they don\'t think about the larger picture. To my mind, a programmer is not an engineer, because an engineer is somebody who starts with a social problem that an organization or a society has and says, "OK, here\'s this problem that we have- how can we solve it?" The engineer comes up with a clever, cost-effective solution to address that problem, builds it, tests it to make sure it solves the problem. That\'s engineering.<i>-Philip Greenspun</i>'
+
+quotes[27]='It should be noted that no ethically -trained software engineer would ever consent to write a DestroyBaghdad procedure. Basic professional ethics would instead require him to write a DestroyCity procedure, to which Baghdad could be given as a parameter. <i>-Nathanial S. Borenstein</i>'
+
+quotes[28]='Let us try to teach generosity and altruism, because we are born selfish. Let us understand what our own selfish genes are up to, because we may then at least have the chance to upset their designs, something that no other species has ever aspired to do. <i>-Richard Dawkins</i>'
+
+quotes[29]='Chance is just a word expressing ignorance. <i>-Richard Dawkins</i>'
+
+quotes[30]='No people come into possession of a culture without having paid a heavy price for it. <i>-James A. Baldwin</i>'
+
+var whichquote=Math.floor(Math.random()*(quotes.length))
+document.write(quotes[whichquote])
